08190fbb46d1d456f23e9f78a18f01586660dd69
khs200 <113793971+khs200@users.noreply.github.com>
2023-02-05 00:34:02 +0900

    css(2)

--- front End/src/App.js
+++ front End/src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import{ BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Login from './login';
 import Signup from './signup';
-import Writing from './writing';
+
 import Main from './main';
 import Eheqhrl from './eheqhrl';
 import Player from './player';
+
 function App() {
   return (
    
     <div className='App'>
+      
 
       
     <BrowserRouter>
@@ -19,12 +21,10 @@ function App() {
       <Route path='/signup' element={<Signup />} />
       <Route path='/main' element={<Main />} />
       <Route path='/player' element={<Player />} />
-      <Route path='/profile' element={<profile />} />
       <Route path='/eheqhrl' element={<Eheqhrl />} />
-      <Route path='/writing' element={<Writing />} />
+
       </Routes>
       </BrowserRouter>
-     
       </div>
   );
 }
